02226e2068b11c7ef0f9e7cad2132407ba9823a4
Eduard Iacob <iacobed2001@gmail.com>
2025-04-21 13:11:55 +0100

    test

--- headmouse-backend/app.py
+++ headmouse-backend/app.py
@@ -7,17 +7,17 @@ app = Flask(__name__)
 CORS(app)
 
 # Open the webcam at startup
-# cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # Use CAP_DSHOW on Windows for faster init
-# if not cap.isOpened():
-#     raise RuntimeError("Cannot open webcam.")
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # Use CAP_DSHOW on Windows for faster init
+if not cap.isOpened():
+    raise RuntimeError("Cannot open webcam.")
 
 @app.route('/')
 def home():
     return jsonify({"message": "Backend is working now EDIIII!"})
 
-# @app.route('/video_feed')
-# def video_feed():
-#     return Response(liveFaceRecognitionTracking(cap), mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/video_feed')
+def video_feed():
+    return Response(liveFaceRecognitionTracking(cap), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
